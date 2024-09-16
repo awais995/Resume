8d9299c9970847e0500a74bac497db45250c8aaa
Muhammmad Awais <awaissoomro65@gmail.com>
2024-09-16 11:18:38 +0500

    Milestone-1-Complete

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
-const toggleSkills = document.getElementById('toggle-skills');
-const skillsSection = document.getElementById('skills');
-toggleSkills?.addEventListener('click', () => {
+var toggleSkills = document.getElementById('toggle-skills');
+var skillsSection = document.getElementById('skills');
+if (toggleSkills && skillsSection) {
+    toggleSkills.textContent = skillsSection.style.display === 'none' ? 'Show Skills' : 'Hide Skills';
+}
+toggleSkills === null || toggleSkills === void 0 ? void 0 : toggleSkills.addEventListener('click', function () {
     if (skillsSection) {
         if (skillsSection.style.display === 'none') {
             skillsSection.style.display = 'block';
+            toggleSkills.textContent = 'Hide Skills';
         }
         else {
             skillsSection.style.display = 'none';
+            toggleSkills.textContent = 'Show Skills';
         }
     }
 });
-const toggleeducation = document.getElementById('toggle-education');
-const educationSection = document.getElementById('education');
-toggleeducation?.addEventListener('click', () => {
+var toggleeducation = document.getElementById('toggle-education');
+var educationSection = document.getElementById('education');
+if (toggleeducation && educationSection) {
+    toggleeducation.textContent = educationSection.style.display === 'none' ? 'Show Skills' : 'Hide Skills';
+}
+toggleeducation === null || toggleeducation === void 0 ? void 0 : toggleeducation.addEventListener('click', function () {
     if (educationSection) {
         if (educationSection.style.display === 'none') {
             educationSection.style.display = 'block';
+            toggleeducation.textContent = 'Hide Skills';
         }
         else {
             educationSection.style.display = 'none';
+            toggleeducation.textContent = 'Show Skills';
         }
     }
 });
-const toggleexperience = document.getElementById('toggle-experience');
-const experienceSection = document.getElementById('work-experience');
-toggleexperience?.addEventListener('click', () => {
+var toggleexperience = document.getElementById('toggle-experience');
+var experienceSection = document.getElementById('work-experience');
+if (toggleexperience && experienceSection) {
+    toggleexperience.textContent = experienceSection.style.display === 'none' ? 'Show Skills' : 'Hide Skills';
+}
+toggleexperience === null || toggleexperience === void 0 ? void 0 : toggleexperience.addEventListener('click', function () {
     if (experienceSection) {
         if (experienceSection.style.display === 'none') {
             experienceSection.style.display = 'block';
+            toggleexperience.textContent = 'Hide Skills';
         }
         else {
             experienceSection.style.display = 'none';
+            toggleexperience.textContent = 'Show Skills';
         }
     }
 });
-export {};
--- app.ts
+++ app.ts
@@ -1,25 +1,39 @@
 const toggleSkills = document.getElementById('toggle-skills');
 const skillsSection = document.getElementById('skills');
 
+if (toggleSkills && skillsSection) {
+    toggleSkills.textContent = skillsSection.style.display === 'none' ? 'Show Skills' : 'Hide Skills';
+}
+
+
 toggleSkills?.addEventListener('click', () => {
     if (skillsSection) {
         if (skillsSection.style.display === 'none') {
             skillsSection.style.display = 'block';
+            toggleSkills.textContent = 'Hide Skills';
         } else {
             skillsSection.style.display = 'none';
+            toggleSkills.textContent = 'Show Skills';
         }
     }
 });
 
+
 const toggleeducation = document.getElementById('toggle-education');
 const educationSection = document.getElementById('education');
+
+if (toggleeducation && educationSection) {
+    toggleeducation.textContent = educationSection.style.display === 'none' ? 'Show Skills' : 'Hide Skills';
+}
 
 toggleeducation?.addEventListener('click', () => {
     if (educationSection) {
         if (educationSection.style.display === 'none') {
             educationSection.style.display = 'block';
+            toggleeducation.textContent = 'Hide Skills'
         } else {
             educationSection.style.display = 'none';
+            toggleeducation.textContent = 'Show Skills'
         }
     }
 });
@@ -27,12 +41,18 @@ toggleeducation?.addEventListener('click', () => {
 const toggleexperience = document.getElementById('toggle-experience');
 const experienceSection = document.getElementById('work-experience');
 
+if (toggleexperience && experienceSection) {
+    toggleexperience.textContent = experienceSection.style.display === 'none' ? 'Show Skills' : 'Hide Skills';
+}
+
 toggleexperience?.addEventListener('click', () => {
     if (experienceSection) {
         if (experienceSection.style.display === 'none') {
             experienceSection.style.display = 'block';
+            toggleexperience.textContent = 'Hide Skills'
         } else {
             experienceSection.style.display = 'none';
+            toggleexperience.textContent = 'Show Skills'
         }
     }
 });
