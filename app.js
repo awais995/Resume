0713ceca69f17fcc15c38ab4739c56f2835f93c0
Wasif Soomro <wasifsoomro761@gmail.com>
2024-09-17 18:54:16 +0500

    Milestone-1

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const toggleSkills = document.getElementById('toggle-skills');
-const skillsSection = document.getElementById('skills');
+var toggleSkills = document.getElementById('toggle-skills');
+var skillsSection = document.getElementById('skills');
 if (toggleSkills && skillsSection) {
     toggleSkills.textContent = skillsSection.style.display === 'none' ? 'Show Skills' : 'Hide Skills';
 }
-toggleSkills?.addEventListener('click', () => {
+toggleSkills === null || toggleSkills === void 0 ? void 0 : toggleSkills.addEventListener('click', function () {
     if (skillsSection) {
         if (skillsSection.style.display === 'none') {
             skillsSection.style.display = 'block';
@@ -15,12 +15,12 @@ toggleSkills?.addEventListener('click', () => {
         }
     }
 });
-const toggleeducation = document.getElementById('toggle-education');
-const educationSection = document.getElementById('education');
+var toggleeducation = document.getElementById('toggle-education');
+var educationSection = document.getElementById('education');
 if (toggleeducation && educationSection) {
     toggleeducation.textContent = educationSection.style.display === 'none' ? 'Show Skills' : 'Hide Skills';
 }
-toggleeducation?.addEventListener('click', () => {
+toggleeducation === null || toggleeducation === void 0 ? void 0 : toggleeducation.addEventListener('click', function () {
     if (educationSection) {
         if (educationSection.style.display === 'none') {
             educationSection.style.display = 'block';
@@ -32,12 +32,12 @@ toggleeducation?.addEventListener('click', () => {
         }
     }
 });
-const toggleexperience = document.getElementById('toggle-experience');
-const experienceSection = document.getElementById('work-experience');
+var toggleexperience = document.getElementById('toggle-experience');
+var experienceSection = document.getElementById('work-experience');
 if (toggleexperience && experienceSection) {
     toggleexperience.textContent = experienceSection.style.display === 'none' ? 'Show Skills' : 'Hide Skills';
 }
-toggleexperience?.addEventListener('click', () => {
+toggleexperience === null || toggleexperience === void 0 ? void 0 : toggleexperience.addEventListener('click', function () {
     if (experienceSection) {
         if (experienceSection.style.display === 'none') {
             experienceSection.style.display = 'block';
@@ -49,4 +49,3 @@ toggleexperience?.addEventListener('click', () => {
         }
     }
 });
-export {};
